543342f8ad37b4b234e7081fff91e0018f663057
oloomoses <olooine@gmail.com>
2020-05-12 23:51:49 +0300

    added the swaping values

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,13 +1,19 @@
-# unsorted_array = [4, 2, 5, 2, 1, 6, 0]
-
 def bubble_sort(my_array)
   size = my_array.length
   size.times do |i|
     j = 0
 
-    while j < size - i
+    while j < size - i do
+        if my_array[j] > my_array[j+1]
+            my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
+        end
+        j += 1
     end
     i += 1
   end
-
+  my_array
 end
+
+unsorted_array = [4, 2, 5, 2, 1, 6, 0] 
+
+p bubble_sort(unsorted_array)
