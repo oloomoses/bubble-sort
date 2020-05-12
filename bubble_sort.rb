863b3c2a707a6cc10d860549be0761acfb4c342d
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-05-12 15:30:17 -0500

    creating the nested loops

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,1 +1,13 @@
-my_array = [4, 2, 5, 2, 1, 6, 0]
+# unsorted_array = [4, 2, 5, 2, 1, 6, 0]
+
+def bubble_sort(my_array)
+  size = my_array.length
+  size.times do |i|
+    j = 0
+
+    while j < size - i
+    end
+    i += 1
+  end
+
+end
