908720c50b5abe017ca4708c9dfad49596be7f5d
oloomoses <olooine@gmail.com>
2020-05-13 17:31:07 +0300

    corrected the linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,17 @@
 def bubble_sort(my_array)
   size = my_array.length
   i = 0
-  while i < size-1 do
+  while i < size - 1
     j = 0
-    while j < size - i - 1 do
-        if my_array[j].to_f > my_array[j+1].to_f
-            my_array[j], my_array[j+1] = my_array[j+1], my_array[j]
-        end
-        j += 1
+    while j < size - i - 1
+      my_array[j], my_array[j + 1] = my_array[j + 1], my_array[j] if my_array[j].to_f > my_array[j + 1].to_f
+      j += 1
     end
     i += 1
   end
   my_array
 end
 
-unsorted_array = [4, 2, 5, 2, 1, 6, 0] 
+unsorted_array = [4, 2, 5, 2, 1, 6, 0]
 
 p bubble_sort(unsorted_array)
