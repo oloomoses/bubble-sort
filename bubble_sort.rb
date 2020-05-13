3c52b21a011d524596d6f341131a94d372485af8
oloomoses <olooine@gmail.com>
2020-05-13 22:29:47 +0300

    bubble_sort_by trial

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -15,22 +15,3 @@
 # unsorted_array = [4, 2, 5, 2.7, 1, 6, 0]
 
 # p bubble_sort(unsorted_array)
-
-def bubble_sort_by( my_array)
-  size = my_array.length
-  i = 0
-  while i < size - 1
-    j = 0
-    while j < size - i - 1
-      # my_array[j], my_array[j + 1] = my_array[j + 1], my_array[j] if (call block that compare arr1 arr2) positif/true
-      my_array[j], my_array[j + 1] = my_array[j + 1], my_array[j] if yield (my_array[j], my_array[j + 1])
-      j += 1
-    end
-    i += 1
-  end
-  my_array
-end
-
-bubble_sort_by do |left, right|
-  left.length - right.length
-end
